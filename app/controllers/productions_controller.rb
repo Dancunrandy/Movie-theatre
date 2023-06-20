789741e2dd0c0dc9dfe8ef93cc3768c4ad11ab75
dancunrandy <dancunrandy@gmail.com>
2023-06-20 09:13:17 +0300

    added update Crud

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,3 +1,41 @@
 class ProductionsController < ApplicationController
-    has_many :crew_members
+  wrap_parameters format: []
+  
+  def index
+    render json: Production.all, status: :ok
+  end
+  
+  def show
+    production = Production.find_by(id: params[:id])
+    if production
+      render json: production, status: :ok
+    else 
+      render json: { error: "Production not found" }, status: :not_found
+    end
+  end
+  
+  def create
+    production = Production.create(production_params)
+    if production.valid?
+      render json: production, status: :created
+    else
+      render json: { error: production.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    production = Production.find_by(id: params[:id])
+    if production
+      production.update(production_params)
+      render json: production, status: :accepted
+    else
+      render json: { error: "Production not found" }, status: :not_found
+    end
+  end
+  
+  private
+  
+  def production_params
+    params.permit(:title, :genre, :budget, :image, :director, :ongoing, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :crew_members
-  resources :productions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :crew_members, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :productions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
